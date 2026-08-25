0fabc33c694c2e4cd4748e868d2b17e403d91207
Faizan Manazir <faizanmanazir0@gmail.com>
2026-08-25 16:02:39 +0500

    Delete backend directory

--- backend/algorithms/utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-import numpy as np
-
-def derive_seed(key: str) -> int:
-    """Derives a deterministic 32-bit integer seed from a string key."""
-    if not key:
-        return 0
-    hash_obj = hashlib.sha256(key.encode('utf-8'))
-    return int.from_bytes(hash_obj.digest()[:4], byteorder='big')
